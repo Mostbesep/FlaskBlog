5f888764da302c5ecbed6a2750be19e24e413494
mostbesep <mostafa.sep57@gmail.com>
2022-03-01 16:24:58 +0330

    initialaze admin.login view

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,5 +1,16 @@
+from flask import session
 from . import admin
 
 @admin.route('/')
 def index():
     return 'hello from admin index'
+
+
+
+@admin.route('/login/')
+def login():
+    session['name'] = 'mostafa'
+    # session.clear()
+    # print(session.get('name'))
+    print(session)
+    return 'login admin'
