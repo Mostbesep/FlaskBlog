ef404c54a540ee7c7b02e6072debdeb1675f70ad
mostbesep <mostafa.sep57@gmail.com>
2022-03-01 21:13:34 +0330

    modify apps modulos for loging

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,4 +1,6 @@
-from flask import session
+from flask import session , render_template , request , abort
+from mod_users.forms import Loginform
+from mod_users.models import User
 from . import admin
 
 @admin.route('/')
@@ -7,10 +9,20 @@ def index():
 
 
 
-@admin.route('/login/')
+@admin.route('/login/' , methods=["GET", 'POST'])
 def login():
-    session['name'] = 'mostafa'
-    # session.clear()
-    # print(session.get('name'))
-    print(session)
-    return 'login admin'
+    form = Loginform(request.form)
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            abort(400)
+        user = User.query.filter(User.email.ilike(f'{form.email.data}')).first() #ilike is a method for compatibility  in databases
+        if not user:
+            return 'incorrect credentials' , 400
+        if not user.check_password(form.password.data):
+            return 'incorrect credentials' , 400
+        session['email'] = user.email
+        session['user_id'] = user.id
+        return 'Logged in successfully'
+    if session.get('email') is not None:
+        return 'you are already logged in'        
+    return render_template('admin/login.html',form = form)
