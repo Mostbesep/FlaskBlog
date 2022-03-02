663a7d7dbdb3b4852e872c33d1bf947dcc42aecc
mostbesep <mostafa.sep57@gmail.com>
2022-03-02 23:33:11 +0330

    prevent from register old users(emails) in blog

--- mod_users/views.py
+++ mod_users/views.py
@@ -1,4 +1,6 @@
 from flask import flash, request , render_template
+#from sqlalchemy.exc import IntegrityError                                 plan b for prevent register old: user sqlachemy IntegrityError
+
 from app import db
 from . import users
 from .forms import Registerform
@@ -14,11 +16,19 @@ def register():
             error_message = 'password and confirm password does not match'
             form.password.errors.append(error_message)
             form.confirm_password.errors.append(error_message)
+        old_user = User.query.filter(User.email.ilike(form.email.data)).first() #plan a for prevent register old user check in database
+        if old_user:                                                            #plan a for prevent register old user check in database
+            flash('email is in use.', category='error')                         #plan a for prevent register old user check in database
+            return render_template('/users/register.html', form = form)         #plan a for prevent register old user check in database
         new_user = User()
         new_user.full_name = form.full_name.data
         new_user.email = form.email.data
         new_user.set_password(form.password.data)
-        db.session.add(new_user)
+        # try:                                                                plan b for prevent register old: user sqlachemy IntegrityError     
+        db.session.add(new_user) #                                                 
         db.session.commit()
         flash('you created your account successfully.', category='success')
+        # except IntegrityError: # if user is aleardy registered              plan b for prevent register old: user sqlachemy IntegrityError
+        #     db.session.rollback()                                           plan b for prevent register old: user sqlachemy IntegrityError
+        #     flash('email is in use.', category='error')                     plan b for prevent register old: user sqlachemy IntegrityError
     return render_template('/users/register.html', form = form)
