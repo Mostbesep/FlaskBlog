4284b5b7b337d801bb293cf8fffc00621e01944c
mostbesep <mostafa.sep57@gmail.com>
2022-03-03 23:44:46 +0330

    create views of blog

--- mod_blog/__init__.py
+++ mod_blog/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 
 
-blog = Blueprint('blog', __name__ , '/blog/')
+blog = Blueprint('blog', __name__ , url_prefix='/blog/')
 
-from . import models
+from . import views
